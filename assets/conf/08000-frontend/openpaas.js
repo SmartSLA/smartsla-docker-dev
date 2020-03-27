4d4f78b0ef228e24e13bfaba99ebb7be4e36c16f
Fabien MOYON <fmoyon@linagora.com>
2020-03-27 15:13:32 +0100

    ticketing08000linux#520 Introduce Support account configuration

--- assets/conf/08000-frontend/openpaas.js
+++ assets/conf/08000-frontend/openpaas.js
@@ -1,4 +1,10 @@
 window.openpaas = {
   VUE_APP_OPENPAAS_URL: "http://backend.ticketing.local",
-  SSP_URL: "http://ssp.ticketing.local"
+  SSP_URL: "http://ssp.ticketing.local",
+  SUPPORT_ACCOUNT: {
+    name: "Linagora",
+    color: "#d32f2f",
+    logo: "logo.png"
+  }
+
 };
