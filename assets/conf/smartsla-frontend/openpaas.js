b4c9dbd111ff9a01056d72c57cc2586efc3ead78
Fabien MOYON <fmoyon@linagora.com>
2022-02-16 18:05:19 +0000

    smartsla-frontend#900 add a reset ssp link

--- assets/conf/smartsla-frontend/openpaas.js
+++ assets/conf/smartsla-frontend/openpaas.js
@@ -1,6 +1,7 @@
 window.openpaas = {
   VUE_APP_OPENPAAS_URL: "http://backend.smartsla.local",
   SSP_URL: "http://ssp.smartsla.local",
+  SSP_URL_RESET: "http://ssp.smartsla.local/reset",
   SUPPORT_ACCOUNT: {
     name: "SmartSLA",
     color: "#d32f2f",
