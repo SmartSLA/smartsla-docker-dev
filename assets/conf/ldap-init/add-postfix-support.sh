15855d2be37d1b07bfb674c5d2c75ecb501c5c4b
Fabien MOYON <fmoyon@linagora.com>
2022-06-08 14:58:07 +0200

    improve webmail

--- assets/conf/ldap-init/add-postfix-support.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ldapadd -x -H ldap:/// -D cn=admin,cn=config -w adminopenpaas -f /linagora/ldifs/postfix.ldif
-
-# ldapmodify -x -H ldap:/// -D cn=admin,dc=openpaas,dc=org -w adminopenpaas -f /linagora/ldifs/modify-postfix-user.ldif
